64c8d9b91d45381a202c058917fe5cacf887c707
Victor110901 <victor.cs2001.vc@gmail.com>
2022-06-30 23:00:14 -0300

    Corrigindo pastas em local errado

--- src/main/java/com/victor/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/victor/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,48 @@
 package com.victor.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victor.helpdesk.domain.Chamado;
+import com.victor.helpdesk.domain.Cliente;
+import com.victor.helpdesk.domain.Tecnico;
+import com.victor.helpdesk.domain.enums.Perfil;
+import com.victor.helpdesk.domain.enums.Prioridade;
+import com.victor.helpdesk.domain.enums.Status;
+import com.victor.helpdesk.repositories.ChamadoRepository;
+import com.victor.helpdesk.repositories.ClienteRepository;
+import com.victor.helpdesk.repositories.TecnicoRepository;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tec1 = new Tecnico(null, "Victor Campolina", "70158757645", "victor.cs2001.vc@gmail.com", "Senha@03042020");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@gmail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
