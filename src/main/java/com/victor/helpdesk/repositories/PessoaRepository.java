6a51d7b74b36109cd4a967b10173568ab86aec0b
victor.campolina <victor.cs2001.vc@gmail.com>
2022-09-08 00:17:07 -0300

    exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/victor/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/victor/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.victor.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.victor.helpdesk.domain.Cliente;
+import com.victor.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Cliente, Integer> {
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
