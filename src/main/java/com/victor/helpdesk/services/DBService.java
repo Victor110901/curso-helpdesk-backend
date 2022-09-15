6718c2349c97bbddb7320953eb00129256663671
victor.campolina <victor.cs2001.vc@gmail.com>
2022-09-14 21:24:12 -0300

    Autenticação com JWT

--- src/main/java/com/victor/helpdesk/services/DBService.java
+++ src/main/java/com/victor/helpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.victor.helpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.victor.helpdesk.domain.Chamado;
@@ -24,13 +25,15 @@ public class DBService {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 
 	public void instanciaDB() {
 		
-		Tecnico tec1 = new Tecnico(null, "Victor Campolina", "70158757645", "victor.cs2001.vc@gmail.com", "Senha@03042020");
+		Tecnico tec1 = new Tecnico(null, "Victor Campolina", "70158757645", "victor@gmail.com", encoder.encode("123"));
 		tec1.addPerfil(Perfil.ADMIN);
 		
-		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@gmail.com", "123");
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@gmail.com", encoder.encode("123"));
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
 		
